2a63fd055a0a039123259ab1eec8cc872748744a
Nerajno <nerajno@gmail.com>
2017-10-01 12:01:01 -0400

    followed the tutorial and figured out how to build the print out

--- assests/js/app.js
+++ assests/js/app.js
@@ -13,7 +13,18 @@ $(document).ready(function() {
      asyc:false,
      dataType: "json",
      success: function(data){
-       console.log(data);
+       // FIXME: look into sending the info out to each list individually
+    //  console.log(data[1][0]); // =>Heading
+       $("#output").prepend(data[1][0]);
+      //  console.log(data[2][0]);//=>Description
+      $("#output").prepend(data[2][0]);
+      //  console.log(data[3][0]);//=>Link
+      $("#output").prepend(data[3][0]);
+
+      for (var i = 0; i < data.length; i++) {
+      $('#output').prepend("<li><a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
+      }
+
      },
      error:function(errorMessage){
        alert("Error");
@@ -22,3 +33,5 @@ $(document).ready(function() {
 
  });
 });
+
+// NOTE: I need to ensure that enter and submit are the same thing.
