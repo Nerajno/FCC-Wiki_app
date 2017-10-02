2bbb92f928e3c00b54df7f69e5bb37a62c802c30
Nerajno <nerajno@gmail.com>
2017-10-02 09:07:26 -0400

    Daily commit

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,37 +1,46 @@
-
 $(document).ready(function() {
 
- $('#search').click(function() {
+  $('#search').click(function() {
 
-   var searchTerm= $('#searchTerm').val();
-   //format is automatically Json
-   var url ="https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm + "&callback=?";
+    var searchTerm = $('#searchTerm').val();
+    //format is automatically Json
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&callback=?";
 
-   $.ajax({
-     type:"GET",
-     url:url,
-     asyc:false,
-     dataType: "json",
-     success: function(data){
-       // FIXME: look into sending the info out to each list individually
-    //  console.log(data[1][0]); // =>Heading
-       $("#output").prepend(data[1][0]);
-      //  console.log(data[2][0]);//=>Description
-      $("#output").prepend(data[2][0]);
-      //  console.log(data[3][0]);//=>Link
-      $("#output").prepend(data[3][0]);
+    $.ajax({
+      type: "GET",
+      url: url,
+      asyc: false,
+      dataType: "json",
+      success: function(data) {
+        // FIXME: look into sending the info out to each list individually
+        //  console.log(data[1][0]); // =>Heading
+        //  $("#output").prepend(data[1][0]);
+        // //  console.log(data[2][0]);//=>Description
+        // $("#output").prepend(data[2][0]);
+        // //  console.log(data[3][0]);//=>Link
+        // $("#output").prepend(data[3][0]);
 
-      for (var i = 0; i < data.length; i++) {
-      $('#output').prepend("<li><a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
+        // NOTE: This clears the old search results when the scripts runs each time.
+        $('#output').html('');
+        //Need to understand this but it wrks
+        for (var i = 0; i < data.length; i++) {
+          $('#output').prepend("<li><a href=" + data[1][i] + ">" + data[3][i] + "</a><p>" + data[2][i] + "</p></li>");
+        }
+        console.log(data);
+        // NOTE: This clears the search term box when the data is delivered.
+      },
+      error: function(errorMessage) {
+        alert("Error, Api not connected");
       }
+    });
 
-     },
-     error:function(errorMessage){
-       alert("Error");
-     }
-   });
+  });
 
- });
+  $("#searchTerm").keypress(function(e) {
+    if (e.which == 13) {
+      $("#search").click();
+    }
+  });
 });
 
-// NOTE: I need to ensure that enter and submit are the same thing.
+// NOTE: I need to ensure that the Enter key and search btn are the same thing.
