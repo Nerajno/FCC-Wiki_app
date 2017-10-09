6b39d54afa068766e20db7ce1929335681956398
Nerajno <nerajno@gmail.com>
2017-10-09 09:49:01 -0400

    tweaked the css and tinkered with the Js

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
+var searchTerm;
 
+$(document).ready(function() {
   $('#search').click(function() {
 
     var searchTerm = $('#searchTerm').val();
@@ -12,27 +13,23 @@ $(document).ready(function() {
       asyc: false,
       dataType: "json",
       success: function(data) {
-        // FIXME: look into sending the info out to each list individually
-        //  console.log(data[1][0]); // =>Heading
-        //  $("#output").prepend(data[1][0]);
-        // //  console.log(data[2][0]);//=>Description
-        // $("#output").prepend(data[2][0]);
-        // //  console.log(data[3][0]);//=>Link
-        // $("#output").prepend(data[3][0]);
 
         // NOTE: This clears the old search results when the scripts runs each time.
         $('#output').html('');
         //Need to understand this but it wrks
-        for (var i = 0; i < data[1].length; i++){
-          $('#output').prepend("<a href=" + data[3][i] + ">" + data[1][i] + "</a><p>" + data[2][i] + '</p> ');
-        }
+        // for (var i = 0; i < data[1].length; i++){
+        //   $('#output').prepend("<a href=" + data[3][i] + ">" + data[1][i] + "</a><p>" + data[2][i] + '</p> ');
+        // }
+        // NOTE: make the for loop to log the data per panel push the header then the p tag then the a href link
         console.log(data);
-        // NOTE: This clears the search term box when the data is delivered.
-
+        console.log(data[1][0], data[2][0], data[3][0]);
+        console.log(data[1][1], data[2][2], data[3][1]);
+      // NOTE: This clears the search term box when the data is delivered.
       },
       error: function(errorMessage) {
         alert("Error, Api not connected");
       }
+
     });
     $("#searchTerm").keyup(function(event) {
       if (event.keyCode ===13) {
