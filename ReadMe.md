cf0d7eee215e336fe0258f4644ef7899c5b8ef4c
Nerajno <nerajno@gmail.com>
2017-10-12 11:44:33 -0400

    fixed the css and addded a ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+Wikipedia Viewer  - Version 0.21
+This is the Wikipedia Viewer  from Free Code Camp's Front Certificate, through it's iteration it has used a couple of ideas and code from other places. The code is abit better than the last challenge and its a bit prettier but it wrks.  
+
+
+## Built With
+- Font Awesome version 4.5.0 and Bootstrap 3.3.6.
+- Contains images from Pexel.org
+- The Wikipedia api
+
+### LICENSE-
+Please feel free to use the this code as you need to.
+This repository adheres to the MIT License.
+
+## Acknowledgments
+Shout out to following person/s and crews :
+-FCC and FCC ATL
+-Esterling and Toby
--- assests/js/app.js
+++ assests/js/app.js
@@ -5,19 +5,16 @@ $(document).ready(function() {
     var searchTerm = $('#searchTerm').val();
     //format is automatically Json
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&callback=?";
-
     $.ajax({
       type: "GET",
       url: url,
       asyc: false,
       dataType: "json",
       success: function(data) {
-
         // NOTE: This clears the old search results when the scripts runs each time.
         $('#output').html('');
-        //Need to understand this but it wrks
         for (var i = 0; i < data[1].length; i++) {
-          //   $('#output').prepend("<a href=" + data[1][i] + ">" + data[2][i] + "</a><p>" + data[3][i] + '</p> ');
+          //push the data to the end.
           $('#output').append("<div class='card'><h2>" + data[1][i] + "</h2><p>" + data[2][i] + "</p><a class='highlight' href='" + data[3][i] + "' target='_blank'>Read More</a></div>");
         }
         console.log(data);
@@ -27,12 +24,11 @@ $(document).ready(function() {
       }
     });
   });
+  // trigger submit on use of enter key
   $("#searchTerm").keypress(function(e) {
     if (e.which === 13) {
       $("#search").click();
       return false;
     }
   });
-  // trigger submit on use of enter key
 });
-// NOTE: I need to ensure that the Enter key and search btn are the same thing.
